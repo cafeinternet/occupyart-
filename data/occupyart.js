99c5cd6627326640ce18d8a38a7061894bd46bf5
Gabriel Zea <zea@randomlab.net>
2013-11-01 14:03:56 -0500

    peticion a database

--- data/occupyart.js
+++ data/occupyart.js
@@ -1,2 +1,20 @@
 self.port.on("replacePage", function(message){
-	$("body").html("<h1> chao mundo</h1>");});
+    $.ajax({
+        url: "http://ocuppytadeo.iriscouch.com/exhibicion/_design/all_works/_view/all_works",
+        type: 'get',
+        dataType: 'jsonp',
+        success: function(data){
+            $.each(data.rows, function(index, element){
+                var autor = element.value.autor;
+                var titulo = element.value.titulo;
+                var img = element.value.img;
+                var comentario = element.value.comentario;
+                                                                    
+                console.log(img);
+                //aca se modifica la pagina
+                $("body").html('<img src="'+img+'">');
+        })
+      }
+    }
+
+});
