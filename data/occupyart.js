9dc680ee537b80dfda31cc3e97f85b62cd951721
Gabriel Zea <zea@randomlab.net>
2013-11-18 16:40:20 -0500

    modificado el codigo

--- data/occupyart.js
+++ data/occupyart.js
@@ -1,20 +1,35 @@
+unsafeWindow.doThings = function(data){
+    $("#preface").append('<ul class="galeria-mod"></ul>');
+    var el = $(".galeria-mod");
+
+    $.each(data.rows, function(index, content){
+        el.append('<li><img src="'+content.value.img+'"></li>')
+
+        if(index == data.total_rows-1){
+            console.log("bang");
+            
+            el.bxSlider();
+        }
+    });
+
+    //aca modificar los CSS
+}
+
+
 self.port.on("replacePage", function(message){
+
+    var el = $("#preface");
+    el.empty();
+
     $.ajax({
         url: "http://ocuppytadeo.iriscouch.com/exhibicion/_design/all_works/_view/all_works",
         type: 'get',
         dataType: 'jsonp',
-        success: function(data){
-            $.each(data.rows, function(index, element){
-                var autor = element.value.autor;
-                var titulo = element.value.titulo;
-                var img = element.value.img;
-                var comentario = element.value.comentario;
-                                                                    
-                console.log(img);
-                //aca se modifica la pagina
-                $("body").html('<img src="'+img+'">');
-        })
-      }
-    }
-
+        //jsonp: 'jsonp',
+        jsonpCallback: 'window.doThings'       
+    });
 });
+
+
+
+
